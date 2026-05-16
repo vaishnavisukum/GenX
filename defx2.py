5de0dfaaf175533f40da01aab98a2486f9fc5744
Vaishnavi Sukum <vaishnavisukum28@gmail.com>
2026-05-16 23:54:25 +0530

    global function ex

--- defx2.py
+++ defx2.py
@@ -1,12 +1,13 @@
-a=10
+a=10           # error will occur 
 def update():
   global a
   a=a+1
   return a
   
-def main():
+def main():        
   no=update()
   print(no)
   
 if __name__=="__main__":
   main()
+ 
